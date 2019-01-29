16f9927d943c067d5a08033f9c9f30035fcc742e
nodemirai <wuyunfeng0@gmail.com>
2019-01-29 10:03:23 +0800

    1. add: readme更新

--- config/inner.js
+++ config/inner.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const componentPath = './src/component'
 const resolveComponentPath = path.resolve(componentPath)
-const modelPath = path.resolve('./src')  //  未来需要更改为外部配置
+const modelPath = path.resolve('./src/pageModel')  //  未来需要更改为外部配置
 const outPath = path.resolve('./dist')
 
 module.exports = {
