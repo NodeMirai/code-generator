b235d94f188d7685c542ae6443f2467bee89f75f
nodemirai <wuyunfeng0@gmail.com>
2019-01-30 21:05:49 +0800

    1. add: 目标添加

--- dist/out.jsx
+++ dist/out.jsx
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import './style.scss';
-import Nav from './src/component';
-import Tab from './src/component';
+import React, { Component } from "react";
+import "./style.scss";
+import Nav from "./src/component";
+import Tab from "./src/component";
 
 class Page extends Component {
   render() {
-    const {
-      text,
-      attr1,
-      attr2,
-      title,
-      attr3,
-      attr4
-    } = this.props;
-    return <div className='demo'><Tab text={text}><Sub attr1={attr1} attr2={attr2}></Sub></Tab>, <Nav title={title}><Item attr3={attr3} attr4={attr4}></Item></Nav>;
-        
-      </div>;
+    const { text, attr1, attr2, title, attr3, attr4 } = this.props;
+    return (
+      <div className="demo">
+        <Tab text={text}>
+          <Sub attr1={attr1} attr2={attr2} />
+        </Tab>
+        ,{" "}
+        <Nav title={title}>
+          <Item attr3={attr3} attr4={attr4} />
+        </Nav>
+        ;
+      </div>
+    );
   }
-
 }
 
 export default Page;
